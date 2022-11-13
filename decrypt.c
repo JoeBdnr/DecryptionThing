28a0cb46301f2053325f0e420b1e7c47a49bd4e1
Mark <joebodner23@gmail.com>
2022-11-13 18:08:08 -0500

    Program can read arguments in the terminal

--- decrypt.c
+++ decrypt.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Hello World");
+int main(int argc, char **argv){
+    char c;
+
+    FILE * textFile = fopen("TextFile.txt", "r");
+    if (textFile){
+        while ((c = getc(textFile)) != EOF)
+            printf("%02X",/*"%c",*/ c);
+        fclose(textFile);
+    }
+    for (int i=1; i< argc; i++) {
+        printf("\narg%d=%s", i, argv[i]);
+    }
+    // argc and argv
+    // atoi 
     return 0;
 }
