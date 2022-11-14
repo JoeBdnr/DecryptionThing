360db1f73d469da42a55c4f68a87e5a54109efc2
Mark <joebodner23@gmail.com>
2022-11-14 00:18:06 -0500

    Can read and write between text files

--- decrypt.c
+++ decrypt.c
@@ -5,39 +5,44 @@
 int main(int argc, char **argv){
     char c;
 
-    FILE * readFile = fopen("TextFile.txt", "r");
-    if (readFile){
-        while ((c = getc(readFile)) != EOF)
-            printf(/*"%02X",*/"%c", c);
-        fclose(readFile);
-    }
-    FILE * writeFile = fopen("TextFile.txt", "w");
-    if (writeFile){
-            // for (char ch = 'a'; ch < 'q'; ch++) /*This one is for going character by character*/
-            //     fputc(ch, textFile);
-            // fprintf(readFile, "Lalala lalalalala lalala lalalalala"); /*This one does a whole line*/
-        fclose(writeFile);
-    }
+/*The two text files need to be changed in a function that will encrypt/decrypt them*/
 
+    FILE * readFile = fopen("TextFile.txt", "r");
+    // if (readFile){
+    //     while ((c = getc(readFile)) != EOF)
+    //         printf(/*"%02X",*/"%c", c);
+    // }
+
+    FILE * writeFile = fopen("TextFile.crp", "w");
+    // if (writeFile){
+    //         // for (char ch = 'a'; ch < 'q'; ch++) /*This one is for going character by character*/
+    //         //     fputc(ch, writeFile);
+    //         fprintf(writeFile, "Message"); /*This one does a whole line*/
+    // }
+
+    // Looks for the first arguement being cryptoMagic and then sees if the user wants to encrypt or decrypt
     if (! strcmp(argv[1], "cryptoMagic")){
-        for (int i=1; i< argc; i++) {
-            if (! strcmp(argv[i], "-E")){
-                /*Encrypt*/
-                printf("Encrypted");
+        if (strcmp(argv[2], "-D") == 0){
+            /*Decrypt*/
+            printf("Decrypted");
+            if (readFile && writeFile){
+                while ((c = getc(readFile)) != EOF){
+                    putc(c, writeFile);
+                }    
             }
-            else if (! strcmp(argv[i], "-D")){
-                /*Decrypt*/
-                printf("Decrypted");
-            }
-            else{
-                /*Encrypt*/
-            }
+        }
+        else{
+            /*Encrypt*/
+            printf("Encrypted");
         }
     }
 
-
+    // This just prints all my arguements
     for (int i=1; i< argc; i++) {
         printf("\narg%d=%s", i, argv[i]);
     }
+
+    fclose(writeFile);
+    fclose(readFile);
     return 0;
 }
