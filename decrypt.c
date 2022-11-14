b0541cb6f77875c3f55375dabc3a799de9c5bcbf
Mark <joebodner23@gmail.com>
2022-11-13 20:57:36 -0500

    Main can read and write to text file, and can see arguments given in the terminal

--- decrypt.c
+++ decrypt.c
@@ -5,16 +5,48 @@
 int main(int argc, char **argv){
     char c;
 
-    FILE * textFile = fopen("TextFile.txt", "r");
+    // FILE * textFile = fopen("TextFile.txt", "r");
+    // if (textFile){
+    //     while ((c = getc(textFile)) != EOF)
+    //         // printf("%02X",/*"%c",*/ c);
+    //         printf("%02X",c);
+    //     fclose(textFile);
+    // }
+    FILE * textFile = fopen("TextFile.txt", "w");
     if (textFile){
-        while ((c = getc(textFile)) != EOF)
-            printf("%02X",/*"%c",*/ c);
+            // for (char ch = 'a'; ch < 'q'; ch++) /*This one is for going character by character*/
+            //     fputc(ch, textFile);
+            fprintf(textFile, "Lalala lalalalala lalala lalalalala"); /*This one does a whole line*/
         fclose(textFile);
     }
+
     for (int i=1; i< argc; i++) {
         printf("\narg%d=%s", i, argv[i]);
     }
-    // argc and argv
-    // atoi 
     return 0;
 }
+
+
+// int main (int argc, char **argv) {
+    
+//     if(argc == 1) {
+//         printf("Need a .foo file");
+//         return 0;
+//     }
+
+//     char *ext = strrchr(argv[1], '.');
+//     if(!ext || strcmp(ext, ".foo")) {
+//         printf("Not a .foo file");
+//         return 0;
+//     }
+
+//     char *basename = (strrchr(argv[1], '\\'));
+//     if(!basename) {
+//         basename = argv[1];
+//     } else {
+//         basename++;
+//     }
+
+//     char *out_file = (char*) malloc((strlen(basename)+1)*sizeof(char));
+//     sprintf(out_file, "%.*s%s", (int) (ext - basename), basename, ".bar");
+// }
